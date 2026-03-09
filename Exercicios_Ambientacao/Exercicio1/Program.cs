f139a011b493f277eff0a2f94aec125e44935c9e
GABRIEL HENRIQUE VILAR DOS SANTOS <gabriel.santos000@cs.up.edu.br>
2026-03-09 20:36:15 -0300

    Criacao dos exercicios e resolucao do ex1

---
+++ Exercicios_Ambientacao/Exercicio1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Exercicio 01
+//Escrever um algoritmo que receba a altura e a largura de um retângulo e calcule a sua área.
+// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double altura;
+        double largura;
+        double area;
+
+        Console.Write("Digite a altura do retângulo: ");
+        altura = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite a largura do retangulo: ");
+        largura = Convert.ToDouble(Console.ReadLine());
+
+        area = altura * largura;
+        Console.WriteLine("A área do retângulo é: " + area);
+
+    }
+}
---
+++ Exercicios_Ambientacao/Exercicio6/Program.cs
@@ -0,0 +1,5 @@
+﻿//Passo a passo para resolver o exercicio
+//1 criar um vetor de valores inteiros com N posicoes
+//2 - Criar um laço de repetição para percorrer o vetor
+//3 - Atribuir um valor aleatorio para cada posicao
+//4- Imprimir o Vetor sem ordenaçao
